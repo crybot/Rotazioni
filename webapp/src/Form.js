e5fe82dc8c78fcb7994b39c649c9074610b2db2f
Marco Pampaloni <m.pampaloni@consorziometis.it>
2021-06-03 19:53:12 +0200

    Add italian localization for days of the week
    - Use <Typography> to render the table's text

--- webapp/src/Form.js
+++ webapp/src/Form.js
@@ -37,12 +37,27 @@ const defaultState = {
 }
 
 function SplitTable(props) {
+
+  function weekday(n) {
+    return {0: 'LUNEDÌ', 1: 'MARTEDÌ', 2: 'MERCOLEDÌ',                          
+      3: 'GIOVEDÌ', 4: 'VENERDÌ', 5: 'SABATO',                          
+      6: 'DOMENICA'}[(n-1) % 7]    
+  }
+
   const numbers = Array.from({length:props.days},(v,k)=>k+1)
   const split = props.split
   const rows = numbers.map((n) => 
     <tr key={n.toString()}> 
-      <td align="left"> {n} </td>
-      <td> {split && n <= split.length ? split[n-1].join(' & ') : ''} </td>
+      <td align="left"> 
+        <Typography>
+          {weekday(n)}
+        </Typography>
+      </td>
+      <td>
+        <Typography>
+          {split && n <= split.length ? split[n-1].join(' & ') : ''}
+        </Typography>
+      </td>
       <td> </td>
       <td> </td>
       <td> </td>
@@ -52,11 +67,27 @@ function SplitTable(props) {
   return (
     <table align="center">
       <tr> 
-        <th> GIORNO </th>
-        <th> ROTAZIONE I </th>
-        <th> ROTAZIONE II </th>
-        <th> ROTAZIONE III </th>
-        <th> RICHIAMO </th>
+        <th> </th>
+        <th>
+          <Typography>
+            ROTAZIONE I 
+          </Typography>
+        </th>
+        <th>
+          <Typography>
+            ROTAZIONE II
+          </Typography>
+        </th>
+        <th>
+          <Typography>
+            ROTAZIONE III
+          </Typography>
+        </th>
+        <th>
+          <Typography>
+            RICHIAMO
+          </Typography>
+        </th>
       </tr>
       {rows}
     </table>
