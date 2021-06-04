8dc2567023e7235b2fb5810a898a4e80291451c4
Marco Pampaloni <m.pampaloni@consorziometis.it>
2021-06-04 12:11:28 +0200

    Move out SplitTable component to its own file

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
-import InputGroup from './InputGroup'
 import { Button, Typography, TextField} from '@material-ui/core';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
+import InputGroup from './InputGroup'
+import SplitTable from './SplitTable'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,64 +42,6 @@ const defaultState = {
   delts_after_rest: false,
 }
 
-function SplitTable(props) {
-
-  function weekday(n) {
-    return {0: 'LUNEDÌ', 1: 'MARTEDÌ', 2: 'MERCOLEDÌ',                          
-      3: 'GIOVEDÌ', 4: 'VENERDÌ', 5: 'SABATO',                          
-      6: 'DOMENICA'}[(n-1) % 7]    
-  }
-
-  const numbers = Array.from({length:props.days},(v,k)=>k+1)
-  const split = props.split
-  const rows = numbers.map((n) => 
-    <tr key={n.toString()}> 
-      <td align="left"> 
-        <Typography>
-          {weekday(n)}
-        </Typography>
-      </td>
-      <td>
-        <Typography>
-          {split && n <= split.length ? split[n-1].join(' & ') : ''}
-        </Typography>
-      </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-    </tr>
-  );
-
-  return (
-    <table align="center">
-      <tr> 
-        <th> </th>
-        <th>
-          <Typography>
-            ROTAZIONE I 
-          </Typography>
-        </th>
-        <th>
-          <Typography>
-            ROTAZIONE II
-          </Typography>
-        </th>
-        <th>
-          <Typography>
-            ROTAZIONE III
-          </Typography>
-        </th>
-        <th>
-          <Typography>
-            RICHIAMO
-          </Typography>
-        </th>
-      </tr>
-      {rows}
-    </table>
-  );
-
-}
 
 function Form(props) {
   const classes = useStyles();
