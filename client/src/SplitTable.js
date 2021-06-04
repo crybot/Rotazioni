ee4c3b3109f087e80997788a8eb29f07ed59ef6f
Marco Pampaloni <m.pampaloni@consorziometis.it>
2021-06-04 13:45:23 +0200

    Implement table filling algorithm
    - Extract SplitCell component

--- client/src/SplitTable.js
+++ client/src/SplitTable.js
@@ -1,5 +1,15 @@
 import { Typography } from '@material-ui/core';
 
+function SplitCell(props) {
+  return (
+    <td>
+      <Typography align="center">
+        {props.groups.join(' & ')} 
+      </Typography>
+    </td>
+  );
+}
+
 function SplitTable(props) {
 
   function weekday(n) {
@@ -8,24 +18,39 @@ function SplitTable(props) {
       6: 'DOMENICA'}[(n-1) % 7]    
   }
 
+  function makeRow(split, n, groups) {
+    var cols = [[], [], []]
+
+    if (split != null) {
+      for (var group in groups) {
+        if (split[n-1].includes(group)) {
+          cols[groups[group]].push(group)
+          groups[group] += 1
+        }
+      }
+    }
+
+    return (
+      <tr key={n.toString()}> 
+        <td align="left"> 
+          <Typography>
+            {weekday(n)}
+          </Typography>
+        </td>
+        <SplitCell groups={cols[0]}/>
+        <SplitCell groups={cols[1]}/>
+        <SplitCell groups={cols[2]}/>
+        <td> </td>
+      </tr>
+    );
+
+  }
+
+  const groups = {'CHEST': 0, 'BACK':0, 'LEGS':0, 'ARMS':0, 'DELTS':0}
   const numbers = Array.from({length:props.days},(v,k)=>k+1)
   const split = props.split
   const rows = numbers.map((n) => 
-    <tr key={n.toString()}> 
-      <td align="left"> 
-        <Typography>
-          {weekday(n)}
-        </Typography>
-      </td>
-      <td>
-        <Typography align="center">
-          {split && n <= split.length ? split[n-1].join(' & ') : ''}
-        </Typography>
-      </td>
-      <td> </td>
-      <td> </td>
-      <td> </td>
-    </tr>
+    makeRow(split, n, groups)
   );
 
   return (
