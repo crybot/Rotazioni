d3f4f999c8c77ea276cb8a123dc1f40f0d3bbc6b
Marco Pampaloni <crybot@hotmail.it>
2021-06-19 00:42:47 +0200

    Add PDF export functionality
    - Use react-to-pdf library to export a non-vectorized screenshot of
    SplitTable

--- client/src/Copyright.js
+++ client/src/Copyright.js
@@ -2,7 +2,7 @@ import { Typography, Link } from '@material-ui/core';
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography style={{marginTop: '30vh'}} variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://github.com/crybot">
         Marco Pampaloni
