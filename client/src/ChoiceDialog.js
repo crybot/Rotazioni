f599b295d7f56e55447b2e72f5aa1daebe3de6c5
Marco Pampaloni <crybot@hotmail.it>
2021-06-29 20:28:58 +0200

    Add a Divider between Richiami and Rest in ChoiceDialog
    - Code refactoring

--- client/src/ChoiceDialog.js
+++ client/src/ChoiceDialog.js
@@ -1,31 +1,12 @@
-import { Dialog, DialogTitle, List, ListItem} from '@material-ui/core';
-import { Button, ListItemText, Typography } from '@material-ui/core';
-import { Chip, Divider, Box, Grid, Icon, IconButton } from '@material-ui/core';
-import { Paper, FormControlLabel, Checkbox} from '@material-ui/core';
+import { Dialog, DialogTitle } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+import { Chip, Divider, Box, Grid, Paper } from '@material-ui/core';
 import { useTheme, makeStyles } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import { globalStyles } from './theme'
 import { mapGroup } from './groups'
 import DoneIcon from '@material-ui/icons/Done';
 import AddIcon from '@material-ui/icons/Add';
-
-function GroupList(props) {
-  return (
-    <List>
-      {
-        props.items.map((item) =>
-          <ListItem onClick={() => props.onItemClick(item)} button key={item}>
-            <ListItemText primary={
-              <Typography align="center" variant="body1">
-                {item.toUpperCase()}
-              </Typography>
-              }/>
-          </ListItem>
-        )
-      }
-    </List>
-  )
-}
 
 function ClickableChip(props) {
   const selected = props.selected
@@ -114,6 +95,20 @@ export default function ChipDialog(props) {
 
   const theme = useTheme()
 
+  const makeChipGroup = (onClick, selected) => {
+    return props.items.map((item) =>
+      <Grid item>
+        <ClickableChip
+          disabled={rest}
+          onClick={() => onClick(item)}
+          selected={selected(item)}
+          className={classes.chip}
+          label={item.toUpperCase()}
+        />
+      </Grid>
+    )
+  }
+
   return (
     <Dialog onClose={handleClose} open={props.open}>
       <Paper style={{backgroundColor: theme.palette.background.default}}>
@@ -122,41 +117,24 @@ export default function ChipDialog(props) {
             <DialogTitle align="center">ROTAZIONI</DialogTitle>
           </Grid>
           {
-            props.items.map((item) =>
-              <Grid item>
-                <ClickableChip
-                  disabled={rest}
-                  onClick={() => handleChipClick(item)}
-                  selected={selected[item]}
-                  className={classes.chip}
-                  label={item.toUpperCase()}
-                />
-              </Grid>
-            )
+            makeChipGroup(item => handleChipClick(item), item => selected[item])
           }
         </Grid>
         <Box align="center">
-          <Divider style={{width: "85%"}}/>
+          <Divider variant="middle"/>
         </Box>
         <Grid container justify="center" className={classes.root}>
           <Grid item xs={12}>
             <DialogTitle align="center">RICHIAMI</DialogTitle>
           </Grid>
           {
-            props.items.map((item) =>
-              <Grid item>
-                <ClickableChip
-                  disabled={rest}
-                  onClick={() => handleRichiamiClick(item)}
-                  selected={richiami[item]}
-                  className={classes.chip}
-                  label={item.toUpperCase()}
-                />
-              </Grid>
-            )
+            makeChipGroup(item => handleRichiamiClick(item), item => richiami[item])
           }
         </Grid>
-        <Box textAlign="center">
+        <Box align="center">
+          <Divider variant="middle"/>
+        </Box>
+        <Box align="center" marginTop="20px">
           <ClickableChip
             onClick={handleRestClick}
             selected={rest}
@@ -166,7 +144,7 @@ export default function ChipDialog(props) {
         </Box>
         <Grid container className={classes.root}>
           <Grid item xs={12}>
-            <Box textAlign="center">
+            <Box align="center">
               <Button
                 className={classes.button}
                 style={{width: "90%"}}
@@ -187,65 +165,3 @@ export default function ChipDialog(props) {
   );
 }
 
-export function SimpleDialog(props) {
-  const { onClose, open, items } = props;
-
-  const handleClose = () => {
-    onClose(null);
-  };
-
-  const handleListItemClick = (value) => {
-    onClose(value);
-  };
-
-  const handleRichiamoClick = (value) => {
-    onClose('richiamo_' + value)
-  }
-
-  const handleDeleteClick = () => {
-    onClose('clear')
-  }
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <Grid container justify="center" alignItems="center" style={{width: "100%"}}>
-
-        <Grid item xs={6}>
-          <DialogTitle align="center">Aggiungi Rotazione</DialogTitle>
-          <Divider disabled={true}/>
-          <GroupList onItemClick={handleListItemClick} items={items}/>
-        </Grid>
-
-        <Grid item xs={6}>
-          <DialogTitle align="center">Aggiungi Richiamo</DialogTitle>
-          <Divider/>
-          <GroupList onItemClick={handleRichiamoClick} items={items}/>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Divider/>
-          <List>
-            <ListItem onClick={() => handleListItemClick('rest')} button key="rest">
-              <ListItemText primary={
-                <Typography align="center" variant="body1">
-                  REST DAY
-                </Typography>
-                }/>
-            </ListItem>
-          </List>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Box textAlign="right">
-            <IconButton onClick={handleDeleteClick}>
-              <Icon>
-                delete
-              </Icon>
-            </IconButton>
-          </Box>
-        </Grid>
-
-      </Grid>
-    </Dialog>
-  );
-}
